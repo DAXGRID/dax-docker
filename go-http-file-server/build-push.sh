6bc12db0ab846dd9d5b4002aaf55ea84b448e5f5
Rune Nielsen <runenielsen@runbox.com>
2021-10-12 16:21:07 +0200

    fixes wrong tag name

--- go-http-file-server/build-push.sh
+++ go-http-file-server/build-push.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-docker build --no-cache -t openftth/go-http-file-server:v1.20.0 -t openftth/go-http-file-server .
+docker build --no-cache -t openftth/go-http-file-server:v1.12.0 -t openftth/go-http-file-server .
 
-docker push openftth/go-http-file-server:v1.20.0
-docker push openftth/inotify-tools:latest
+docker push openftth/go-http-file-server:v1.12.0
+docker push openftth/go-http-file-server:latest
