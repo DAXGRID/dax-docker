d627781a6dce8d65bb0aa935378025f36aba673c
Rune Nielsen <runenielsen@runbox.com>
2022-11-09 14:04:43 +0100

    update go http file server to version v1.15.5

--- go-http-file-server/build-push.sh
+++ go-http-file-server/build-push.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
-set -e
+docker build --no-cache -t openftth/go-http-file-server:v1.15.5 -t openftth/go-http-file-server .
 
-docker build --no-cache -t openftth/go-http-file-server:v1.12.0 -t openftth/go-http-file-server .
-
-docker push openftth/go-http-file-server:v1.12.0
+docker push openftth/go-http-file-server:v1.15.5
 docker push openftth/go-http-file-server:latest
