08890f05169d5ac2481481b0d2078ac5f43bf1f2
Rune Nielsen <runenielsen@runbox.com>
2022-11-18 09:09:20 +0100

    simplifies the image setup

--- go-http-file-server/build-push.sh
+++ go-http-file-server/build-push.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-docker build --no-cache -t openftth/go-http-file-server:v1.15.5 -t openftth/go-http-file-server .
+docker build --no-cache -t openftth/go-http-file-server:v1.15.5_v2 -t openftth/go-http-file-server .
 
-docker push openftth/go-http-file-server:v1.15.5
+docker push openftth/go-http-file-server:v1.15.5_v2
 docker push openftth/go-http-file-server:latest
