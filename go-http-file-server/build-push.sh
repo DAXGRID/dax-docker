e5cb44e182a29dd71fbb7b62aa05d2fb9bbe91db
Rune Nielsen <runenielsen@runbox.com>
2022-11-20 14:04:05 +0100

    now uses entrypoint instead of cmd to not use shell mode

--- go-http-file-server/build-push.sh
+++ go-http-file-server/build-push.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-docker build --no-cache -t openftth/go-http-file-server:v1.15.5_v2 -t openftth/go-http-file-server .
+docker build --no-cache -t openftth/go-http-file-server:v1.15.5_v3 -t openftth/go-http-file-server .
 
-docker push openftth/go-http-file-server:v1.15.5_v2
+docker push openftth/go-http-file-server:v1.15.5_v3
 docker push openftth/go-http-file-server:latest
