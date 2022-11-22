b4dfe693be5995f2e517a28e161a6627e541c8ed
Rune Nielsen <runenielsen@runbox.com>
2022-11-22 21:37:02 +0100

    improve formatting for building docker image

--- go-http-file-server/build-push.sh
+++ go-http-file-server/build-push.sh
@@ -4,7 +4,10 @@ set -e
 
 TAG_VERSION="v1.15.5_v4"
 
-docker build --no-cache -t openftth/go-http-file-server:$TAG_VERSION -t openftth/go-http-file-server .
+docker build --no-cache \
+       -t openftth/go-http-file-server:$TAG_VERSION \
+       -t openftth/go-http-file-server:latest \
+       .
 
 docker push openftth/go-http-file-server:$TAG_VERSION
 docker push openftth/go-http-file-server:latest
