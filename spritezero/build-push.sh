82255e4225fcf17d163cbe1ff68e454e7c8ad114
Rune Nielsen <runenielsen@runbox.com>
2022-11-22 21:07:42 +0100

    now also pushes latest tag for spirte zero

--- spritezero/build-push.sh
+++ spritezero/build-push.sh
@@ -2,8 +2,12 @@
 
 set -e
 
-VERSION=v1.1.0
+VERSION_TAG="v1.1.0"
 
-docker build --no-cache -t openftth/spritezero-cli:$VERSION  .
+docker build --no-cache \
+       -t openftth/spritezero-cli:$VERSION_TAG \
+       -t openftth/spritezero-cli:latest \
+       .
 
-docker push openftth/spritezero-cli:$VERSION
+docker push openftth/spritezero-cli:$VERSION_TAG
+docker push openftth/spritezero-cli:latest
