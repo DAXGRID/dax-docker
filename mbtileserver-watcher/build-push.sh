1b40ab4696923e2f23d6fd88e1d16ea54d928767
Rune Nielsen <runenielsen@runbox.com>
2022-11-22 20:50:02 +0100

    remove mbtileserver watcher no longer used in projects

--- mbtileserver-watcher/build-push.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-docker build --no-cache -t openftth/mbtileserver-watcher:v1.1.0 -t openftth/mbtileserver-watcher:latest .
-
-docker push openftth/mbtileserver-watcher:v1.1.0
-docker push openftth/mbtileserver-watcher:latest
