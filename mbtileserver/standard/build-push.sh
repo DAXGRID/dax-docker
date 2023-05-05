c1be08caebe6fa6046939a70a50c48469bfc2555
Rune Nielsen <runenielsen@runbox.com>
2023-05-05 11:07:38 +0200

    fix issue used older version for mbtileserver

--- mbtileserver/standard/build-push.sh
+++ mbtileserver/standard/build-push.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-docker build --no-cache -t openftth/mbtileserver:v0.9.0 -t openftth/mbtileserver:latest .
+docker build --no-cache -t openftth/mbtileserver:v0.9.0-v2 -t openftth/mbtileserver:latest .
 
-docker push openftth/mbtileserver:v0.9.0
+docker push openftth/mbtileserver:v0.9.0-v2
 docker push openftth/mbtileserver:latest
