5d344ba456bae8f5ca101b771fb80623a6df4e09
Rune Nielsen <runenielsen@runbox.com>
2023-05-05 10:07:33 +0200

    update mbtileserver to version v0.9.0

--- mbtileserver/standard/build-push.sh
+++ mbtileserver/standard/build-push.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-docker build --no-cache -t openftth/mbtileserver:v0.7.0 -t openftth/mbtileserver:latest .
+docker build --no-cache -t openftth/mbtileserver:v0.9.0 -t openftth/mbtileserver:latest .
 
-docker push openftth/mbtileserver:v0.7.0
+docker push openftth/mbtileserver:v0.9.0
 docker push openftth/mbtileserver:latest
