55ec7f2170db7d5858b9625a5a2ac1e8e00c058d
Rune Nielsen <runenielsen@runbox.com>
2023-07-20 15:26:48 +0200

    can now process danish basemap, no longer gets data from danish ftp

--- mbtileserver/danish/build-push.sh
+++ mbtileserver/danish/build-push.sh
@@ -8,50 +8,8 @@ trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 # echo an error message before exiting
 trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
 
-kortforsyning_user=$1
-kortforsyning_password=$2
-
 # Create tmp folder that we can store tmp data in
 mkdir -p ./tmp
-
-# Get Vejmidte
-wget ftp://$kortforsyning_user:$kortforsyning_password@ftp.kortforsyningen.dk//grundlaeggende_landkortdata/landsdaekkende-vejdata/Vejmidte-GPKG_UTM32-ETRS89.zip -O ./tmp/veje.zip
-unzip -j "./tmp/veje.zip" "vejmidte_navn.gpkg" -d "./tmp/veje"
-ogr2ogr -f GeoJSON -sql "SELECT geometri, 'vejmidte' as objecttype, vejnavn FROM vejmidte_navn" -append -t_srs crs:84 ./tmp/danish-basemap.geojson ./tmp/veje/vejmidte_navn.gpkg -nln danish-basemap
-
-# Skel
-wget ftp://$kortforsyning_user:$kortforsyning_password@ftp.kortforsyningen.dk//matrikeldata/matrikelkort/MAPINFO/DK_MAPINFO_UTM32-EUREF89.zip -O ./tmp/matrikel.zip
-unzip -j "./tmp/matrikel.zip" "DK_MAPINFO_UTM32-EUREF89/MINIMAKS/BASIS/SKEL.*" -d "./tmp/matrikel"
-ogr2ogr -f GeoJSON -sql "SELECT 'skel' as objecttype FROM skel" -append -t_srs crs:84 ./tmp/danish-basemap.geojson ./tmp/matrikel/SKEL.tab -nln danish-basemap
-
-# Get data for Vejkant & Helle & Nedloebsrist & Broenddaeksel & Lysmast
-wget ftp://$kortforsyning_user:$kortforsyning_password@ftp.kortforsyningen.dk//grundlaeggende_landkortdata/fot/MAPINFO/DK_MAPINFO_UTM32-EUREF89.zip -O ./tmp/fot.zip
-
-# # Vejkant
-unzip -j "./tmp/fot.zip" "DK_MAPINFO_UTM32-EUREF89/FOT/TRAFIK/VEJKANT.*" -d "./tmp/fot"
-ogr2ogr -f GeoJSON -sql "SELECT 'vejkant' as objecttype FROM vejkant" -append -t_srs crs:84 ./tmp/danish-basemap.geojson ./tmp/fot/VEJKANT.tab -nln danish-basemap
-
-# # Helle
-unzip -j "./tmp/fot.zip" "DK_MAPINFO_UTM32-EUREF89/FOT/TRAFIK/HELLE.*" -d "./tmp/fot"
-ogr2ogr -f GeoJSON -sql "SELECT 'helle' as objecttype FROM helle" -append -t_srs crs:84 ./tmp/danish-basemap.geojson ./tmp/fot/HELLE.tab -nln danish-basemap
-
-# Nedloebsrist
-unzip -j "./tmp/fot.zip" "DK_MAPINFO_UTM32-EUREF89/FOT/TEKNIK/NEDLOEBSRIST.*" -d "./tmp/fot"
-ogr2ogr -f GeoJSON -sql "SELECT 'nedloebsrist' as objecttype FROM nedloebsrist" -append -t_srs crs:84 ./tmp/danish-basemap.geojson ./tmp/fot/NEDLOEBSRIST.tab -nln danish-basemap
-
-# Broenddaeksel
-unzip -j "./tmp/fot.zip" "DK_MAPINFO_UTM32-EUREF89/FOT/TEKNIK/BROENDDAEKSEL.*" -d "./tmp/fot"
-ogr2ogr -f GeoJSON -sql "SELECT 'broenddaeksel' as objecttype FROM broenddaeksel" -append -t_srs crs:84 ./tmp/danish-basemap.geojson ./tmp/fot/BROENDDAEKSEL.tab -nln danish-basemap
-
-# Lysmast
-unzip -j "./tmp/fot.zip" "DK_MAPINFO_UTM32-EUREF89/FOT/TEKNIK/MAST.*" -d "./tmp/fot"
-ogr2ogr -f GeoJSON -sql "SELECT 'mast' as objecttype FROM mast" -append -t_srs crs:84 ./tmp/danish-basemap.geojson ./tmp/fot/MAST.tab -nln danish-basemap
-
-# Create mbtiles
-docker run -it --rm \
-  -v $(pwd)/tmp:/data \
-  openftth/tippecanoe:latest \
-  tippecanoe --minimum-zoom=16 --maximum-zoom=16 --force --output=/data/objects.mbtiles /data/danish-basemap.geojson
 
 # Get openmaptiles to generate danish basemap
 git clone -b v3.12.2 https://github.com/openmaptiles/openmaptiles.git ./tmp/openmaptiles
@@ -61,16 +19,12 @@ cp -rf ./env-openmaptiles ./tmp/openmaptiles/.env
 
 # Create tilesets folder for tilesets and move tiles to it
 mkdir -p ./tilesets
-cp ./tmp/objects.mbtiles ./tilesets/
 cp ./tmp/openmaptiles/data/tiles.mbtiles ./tilesets/osm.mbtiles
 
-# Remove tmp folder to increase docker build speed
+# Remove tmp folder to decrease the image size.
 rm -rf ./tmp
 
 # Build docker image with timestamp and push it
 timestamp=$(date +%s)
 docker build -t openftth/mbtileserver:danish-$timestamp .
 docker push openftth/mbtileserver:danish-$timestamp
-
-# Remove tilesets since they're no longer needed
-rm -rf ./tilesets/
