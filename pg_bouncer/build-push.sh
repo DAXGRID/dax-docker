0ca80229a26bfa7bf9a042a1361d1117e12f47fc
runeanielsen <runenielsen@runbox.com>
2025-03-16 19:19:52 +0100

    add pg_bouncer image

---
+++ pg_bouncer/build-push.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+
+VERSION_TAG="1.15.0-1"
+IMAGE_NAME="daxgrid/pg_bouncer"
+
+docker build --no-cache \
+       -t $IMAGE_NAME:$VERSION_TAG \
+       -t $IMAGE_NAME:latest \
+       .
+
+docker push $IMAGE_NAME:$VERSION_TAG
+docker push $IMAGE_NAME:latest
