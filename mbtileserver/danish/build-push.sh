68919d91ed5323bd0fce14036475c0cdcb67338f
runeanielsen <runenielsen@runbox.com>
2026-09-12 11:33:36 +0200

    switch to using newest images

--- mbtileserver/danish/build-push.sh
+++ mbtileserver/danish/build-push.sh
@@ -6,7 +6,7 @@ set -e
 # keep track of the last executed command
 trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 # echo an error message before exiting
-trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
+trap 'echo "\"${last_command}\" command finished with exit code $?."' EXIT
 
 # Create tmp folder that we can store tmp data in
 mkdir -p ./tmp
